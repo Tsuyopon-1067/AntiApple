3282fcbdb7176b7c138faac1ab37c36b049ce2bd
Tsuyopon-1067 <91947809+Tsuyopon-1067@users.noreply.github.com>
2022-12-03 21:24:17 +0900

    Create CarouselImages.jsx to divide the part defining the images.

---
+++ anti-apple/src/components/CarouselImages.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Image1png from './CarouselImages/1.png';
+import Image2png from './CarouselImages/2.png';
+import Image3png from './CarouselImages/3.png';
+import Image from '@chakra-ui/react';
+
+export const Image1 = (props) => {
+    return (
+      <Image src={Image1png} />
+    );
+};
+export const Image2 = (props) => {
+    return (
+      <Image src={Image2png} />
+    );
+};
+export const Image3 = (props) => {
+    return (
+      <Image src={Image3png} />
+    );
+};
