591d696317ada19badf4c23bb9e5883d4a0d4dbd
Tsuyopon-1067 <91947809+Tsuyopon-1067@users.noreply.github.com>
2022-12-03 21:34:45 +0900

    Fix bug of import.

--- anti-apple/src/components/CarouselImages.jsx
+++ anti-apple/src/components/CarouselImages.jsx
@@ -2,19 +2,19 @@ import React from 'react';
 import Image1png from './CarouselImages/1.png';
 import Image2png from './CarouselImages/2.png';
 import Image3png from './CarouselImages/3.png';
-import Image from '@chakra-ui/react';
+import {Image} from '@chakra-ui/react';
 
-export const Image1 = (props) => {
+export const Image1 = () => {
     return (
       <Image src={Image1png} />
     );
 };
-export const Image2 = (props) => {
+export const Image2 = () => {
     return (
       <Image src={Image2png} />
     );
 };
-export const Image3 = (props) => {
+export const Image3 = () => {
     return (
       <Image src={Image3png} />
     );
