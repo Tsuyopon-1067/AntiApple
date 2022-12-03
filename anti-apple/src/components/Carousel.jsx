71963fb8bd32f2c156c44441ff0cca05447779f9
Tsuyopon-1067 <91947809+Tsuyopon-1067@users.noreply.github.com>
2022-12-03 12:15:19 +0900

    Create carousel files (Jjsx css).

---
+++ anti-apple/src/components/Carousel.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Styles from './Carousel.module.css';
+
+export default function Carousel() {
+    return (
+        <React.Fragment>
+        </React.Fragment>
+    )
+}
