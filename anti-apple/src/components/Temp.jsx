5da8c3463c3e931f1c4db27dc0201a5a72289b2c
Tsuyopon-1067 <91947809+Tsuyopon-1067@users.noreply.github.com>
2022-12-05 03:41:56 +0900

    Revert "Delete Temp.jsx"
    This reverts commit 731def4314b1110ef642c95f16bf762906c37265.

---
+++ anti-apple/src/components/Temp.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { Image } from '@chakra-ui/react'
+
+
+export const Mekw = (props) => {
+    return (
+        <Image src='https://upload.wikimedia.org/wikipedia/commons/b/b8/Shinkansen_N700_z15.jpg' />
+    );
+};
