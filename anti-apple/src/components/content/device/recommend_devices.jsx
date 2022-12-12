32fbff186ccf5049b02502bd1c995800e43d7789
Tsuyopon-1067 <91947809+Tsuyopon-1067@users.noreply.github.com>
2022-12-12 18:58:04 +0900

    Add phones.

--- anti-apple/src/components/content/device/recommend_devices.jsx
+++ anti-apple/src/components/content/device/recommend_devices.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Device} from '../../Device'; // デバイスコンポーネント
-import {Pixel6a} from '../../Devices'; // 端末情報定義（端末追加のときはこっちをいじる）．波括弧内に端末を追加する．
+import {Pixel6a, Xiaomi11TPro, Redminote11, iPhoneSE } from '../../Devices'; // 端末情報定義（端末追加のときはこっちをいじる）．波括弧内に端末を追加する．
 
 const RecoDevices = () => {
     return (
@@ -9,6 +9,9 @@ const RecoDevices = () => {
             <p>おすすめ端末1~10</p>
                 {/* ここでテスト用デバイス画面を配置 */}
                 <Device {...Pixel6a} />
+                <Device {...Xiaomi11TPro} />
+                <Device {...Redminote11} />
+                <Device {...iPhoneSE} />
         </div>
     );
 };
