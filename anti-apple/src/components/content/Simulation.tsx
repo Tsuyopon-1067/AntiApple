e25a46b12eff0c0ccba9044b003f2b85a426ead1
Tsuyopon-1067 <91947809+Tsuyopon-1067@users.noreply.github.com>
2022-12-20 18:38:24 +0900

    feat: Add option button.
    オプション選択ボタンを追加した．

--- anti-apple/src/components/content/Simulation.tsx
+++ anti-apple/src/components/content/Simulation.tsx
@@ -2,16 +2,23 @@ import React, { useState } from 'react';
 import { Plan, Plans } from './SimulationComponents/Plans';
 import {SimulationSlider} from './SimulationComponents/SimulationSlider';
 import {CarrierButtonList} from './SimulationComponents/CarrierButtonList';
+import {OptionButtonList} from './SimulationComponents/OptionButtonList ';
 // 20 50 70 100
 export const Simulation = (param) => {
     const[dataVolume, setDataVolume] = useState(5);
     const[carrier, setCarrier] = useState([1]);
+    const[option, setOption] = useState([1]);
     let selectedPlan: Plan = Plans.getSelectedPlan(carrier, dataVolume, 0);
     return (
         <React.Fragment>
             <h1>プラン料金シミュレーション</h1>
             <p>ここに料金シミュレーションを実装</p>
-            <CarrierButtonList getCarrier={setCarrier} />
+            <p>
+                <CarrierButtonList getCarrier={setCarrier} />
+            </p>
+            <p>
+                <OptionButtonList getCarrier={setOption} />
+            </p>
             <SimulationSlider setDataVolume={setDataVolume} />
             <br />
             <p>選択 : {dataVolume}</p>
---
+++ anti-apple/src/components/content/SimulationComponents/OptionButtonList .tsx
@@ -0,0 +1,25 @@
+// キャリア選択ボタン
+import React, { useEffect, useState } from 'react';
+import  { ToggleButton } from './ToggleButton';
+
+export const OptionButtonList = (props) => {
+    const [isChecked0, setIsChecked0] = useState(false);
+    const [isChecked1, setIsChecked1] = useState(false);
+    const [isChecked2, setIsChecked2] = useState(false);
+
+    useEffect(() => {
+        let lst: number[] = new Array();
+        if (isChecked0) { lst.push(0) }
+        if (isChecked1) { lst.push(1) }
+        if (isChecked2) { lst.push(2) }
+        props.getCarrier(lst);
+    }, [isChecked0, isChecked1, isChecked2]);
+
+    return(
+        <React.Fragment>
+            <ToggleButton getState={setIsChecked0}>無制限通話</ToggleButton>
+            <ToggleButton getState={setIsChecked1}>通話（時間制限あり）</ToggleButton>
+            <ToggleButton getState={setIsChecked2}>なにか</ToggleButton>
+        </React.Fragment>
+    );
+}
