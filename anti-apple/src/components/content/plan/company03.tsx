3038a9a48ff2f604fda75f3642120a092783a097
Roshiori <brzrokuniyon@gmail.com>
2023-01-19 00:04:20 +0900

    update: article generate

--- anti-apple/src/components/content/plan/company03.tsx
+++ anti-apple/src/components/content/plan/company03.tsx
@@ -1,12 +1,54 @@
-import React from 'react';
+import React from "react";
+import GenerateArticle from "./generateArticle";
 
 const company03 = () => {
-    return (
-        <div>
-            <h1>LINEMO</h1>
-            <p>最高の格安SIM</p>
-        </div>
-    );
+  return (
+    <div>
+      <GenerateArticle
+        name="LINEMO"
+        img="https://www.linemo.jp/assets/images/common/logo.svg"
+        abstract="LINEMOは、ソフトバンクが提供しているオンライン限定の格安プランです。比較的通信速度が早く、昼間でも安定して使えます。"
+        gbtable={["3GB", "20GB"]}
+        valuetable={[990, 2728]}
+        valueremarktable={[
+          "データ容量超過後の通信速度は300Kbps, LINEスタンププレミアム for LINEMO非対応",
+          "データ容量超過後の通信速度は1Mbps",
+        ]}
+        value="プラン構成は、低容量の3GBと20GBの二形態です。特筆すべきは3GBミニプランのコスパの良さで、大手通信キャリアのソフトバンクとほぼ変わらない品質の回線が月額990円という安さで使えます。どちらもLINEカウントフリーを採用しており、LINEは通話を含めいくら使用してもデータ容量を消費しません。ただし、データ容量超過後の通信速度は3GBが300Kbps、20GBが1Mbpsになる点は要注意です。"
+        optiontable={[
+          "5分以内かけ放題",
+          "かけ放題",
+          "LINE Music for SoftBank",
+          "持込端末保証 with AppleCare Services",
+          "持込端末保証",
+          "留守電パック",
+          "+メッセージ",
+          "セキュリティパックプラス(L)",
+          "フィルタリング",
+          "世界対応ケータイ",
+          "国際電話",
+        ]}
+        optionvaluetable={[550, 1650, 980, 1480, 715, 220, 0, 660, 0, 0, 0]}
+        optionremarktable={[
+          "国内に限る",
+          "国内に限る",
+          "学生は480円",
+          "iPhone 14 Proの場合。iPhoneのモデルによって料金は異なります。",
+          "Android端末もしくはiPad向け",
+          "",
+          "",
+          "",
+          "",
+          "オプション加入料金と別途にデータサービス定額料金が発生します。",
+          "オプション加入料金と別途に通話料が発生します。",
+        ]}
+        option="LINEMOはオプションも充実しています。別途通信料金はかかりますが海外ローミングも可能で、海外に出かける用事の多い方のメイン回線としても便利です。"
+        speed="LINEMOは比較的速度が速いです。格安SIMの中には昼間の速度が非常に安定しないものもありますが、LINEMOは運営元が大手キャリアのソフトバンクであるため昼間でも安定して速度が出ます。ストレスなくいつでも使いたい方におすすめです。"
+        recommended="LINEでよく通話をする人や、通信品質の安定感を求めている方におすすめです。その性質ゆえ、メイン回線としても非常に扱いやすいでしょう。ただし、PayPayをよく使われる方や店頭サポートを求める方は料金自体は割高になりますが同じソフトバンク系のY!mobileがおすすめです。"
+        reference="【公式】LINEMO｜ミニプランが最大半年間基本料実質無料｜格安SIM／格安スマホ (https://www.linemo.jp/)"
+      />
+    </div>
+  );
 };
 
 export default company03;
--- anti-apple/src/components/content/plan/generateArticle.tsx
+++ anti-apple/src/components/content/plan/generateArticle.tsx
@@ -1,27 +1,74 @@
-import React from 'react';
+import React from "react";
 //import ReactDOM from 'react-dom';
+import {
+  Table,
+  Thead,
+  Tbody,
+  Tfoot,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+  TableContainer,
+} from "@chakra-ui/react";
 
 const GenerateArticle = (props) => {
-    return (
-        <div>
-            <h1>{props.name}</h1>
-            <img src={props.img} />
-            <h2>概要</h2>
-            <p>{props.abstract}</p>
-            <h2>データ通信容量</h2>
-            <p>{props.gb}</p>
-            <h2>料金プラン</h2>
-            <p>{props.value}</p>
-            <h2>オプション</h2>
-            <p>{props.option}</p>
-            <h2>通信速度</h2>
-            <p>{props.speed}</p>
-            <h2>どんな人におすすめ？</h2>
-            <p>{props.recommended}</p>
-            <h2>参考文献</h2>
-            <p>{props.reference}</p>
-        </div>
-    );
+    const valuedata : JSX.Element[] = []
+    const optiondata : JSX.Element[] = []
+    for(let i = 0; i < props.gbtable.length; i++){
+        valuedata.push(<Tr><Td>{props.gbtable[i]}</Td><Td>{props.valuetable[i]+"円"}</Td><Td>{props.valueremarktable[i]}</Td></Tr>)
+    }
+    for (let j = 0; j < props.optiontable.length; j++){
+        optiondata.push(<Tr><Td>{props.optiontable[j]}</Td><Td>{props.optionvaluetable[j]+"円"}</Td><Td>{props.optionremarktable[j]}</Td></Tr>)
+    }
+  return (
+    <div>
+      <h1>{props.name}</h1>
+      <img src={props.img} />
+      <h2>概要</h2>
+      <p>{props.abstract}</p>
+      <h2>プラン</h2>
+      <TableContainer>
+        <Table variant="simple">
+          <TableCaption>月額、料金は税込です</TableCaption>
+          <Thead>
+            <Tr>
+              <Th>データ容量</Th>
+              <Th>料金</Th>
+              <Th>備考</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {valuedata}
+          </Tbody>
+        </Table>
+      </TableContainer>
+      <p>{props.value}</p>
+      <h2>オプション</h2>
+      <TableContainer>
+        <Table variant="simple">
+          <TableCaption>月額、料金は税込です</TableCaption>
+          <Thead>
+            <Tr>
+              <Th>オプション</Th>
+              <Th>料金</Th>
+              <Th>備考</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {optiondata}
+          </Tbody>
+        </Table>
+      </TableContainer>
+      <p>{props.option}</p>
+      <h2>通信速度</h2>
+      <p>{props.speed}</p>
+      <h2>まとめ・どんな人におすすめ？</h2>
+      <p>{props.recommended}</p>
+      <h2>参考文献</h2>
+      <p>{props.reference}</p>
+    </div>
+  );
 };
 
 export default GenerateArticle;
